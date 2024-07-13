86aa3e694b7305c1aa57615450afefc334e4c94d
Nuff3d <lucaseduardocampos457@gmail.com>
2024-07-12 23:14:20 -0300

    Modificação e implementação da lib pyautogui

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World - Ainda em teste")
+import pandas as pd
+import pyautogui
+
+
+caminho_absoluto = '/root/projetosTESTE/projetos/euvendedor/euvendedor/aniversarioclienteschevrolet.xlsx'
+
+planilha = pd.read_excel(caminho_absoluto)
+
+print(planilha)
+
