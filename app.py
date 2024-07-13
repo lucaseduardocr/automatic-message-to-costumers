063515baa770f282a98ea615df36f3ba1111cabc
Lucas Eduardo <lucaseduardocampos457@gmail.com>
2024-07-13 01:24:51 -0300

    Troca de simulação de Máquina do Ubunto para Windows

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import pyautogui
+import tkinter
+import webbrowser
+import datetime
+
+data_atual = datetime.date.today()
+data_atual_formatada = data_atual.strftime("%d/%m/%Y")
 
 
-caminho_absoluto = '/root/projetosTESTE/projetos/euvendedor/euvendedor/aniversarioclienteschevrolet.xlsx'
+caminho_absoluto = r'C:\Users\lucas\Projetos\euvendedor\aniversarioclienteschevrolet.xlsx'
 
 planilha = pd.read_excel(caminho_absoluto)
 
-print(planilha)
-
+for indice, linha in planilha.iterrows():
+    data_aniversario = linha[2]
+    print(data_aniversario)
